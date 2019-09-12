05740192bb8de1c0e8461e97cfd54a3bbc7d2991
cieraml <c00034347@louisiana.edu>
2019-09-11 23:31:08 -0500

    hw2

--- HW2.R
+++ HW2.R
@@ -5,6 +5,34 @@ c = as.integer(19)
 d = 22.5
 food = c("pizza","wings","nachos")
 ffood = factor(food)
+a^2
+#aisbeingsquaredby2
+a%%b
+#gives the remainder of division
+b%/%a
+#integer division
+g <- (1:10)
+x = 4
+y = 5
+z = 6
+x*y+z
+x*(y+z)
+#The output for x*y+z is 26 because you multiply 4 & 5 then add 6.
+#The output for x*(y+z) is 44 because starting with the parenthesis, 5 + 6 is 11 & that multiplied by 4 is 44.
+v <- c(1,2,3)
+length(v) <- 4
+k <- 1/0
+m <- -1/0
+0/0
+as.null(list(food))
+"%cats%" <- function (x,y){x+y}
+2 %/% 2
+18 %% 4
+4^2
+animals <- c("giraffe","dog","horse","panda")
+if("giraffe" %in% animals){
+  print(TRUE)
+} else {
+  print(FALSE)
+}
 
-#watch this 
-#practive 
